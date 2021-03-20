af5f3c506d6c82d66bc522df1318c0cbcfd1f82d
Supun608 <80893855+Supun608@users.noreply.github.com>
2021-03-20 19:01:35 +0530

    index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ CFonts.say('Rendy Gans', {
   align: 'center',
   gradient: ['red', 'magenta']
 })
-CFonts.say('Jika Ada Bug Bisa Hubungi\nwa.me/62895337278647', {
+CFonts.say('Jika Ada Bug Bisa Hubungi\nwa.me/94754273991', {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
